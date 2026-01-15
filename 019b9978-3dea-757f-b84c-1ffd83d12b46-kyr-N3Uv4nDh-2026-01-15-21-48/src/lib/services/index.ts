182f03f352969fd49cd43c8d42485e22ad89f92b
contact699 <contact@growyoursb.com>
2026-01-15 17:20:30 -0500

    Delete 019b9978-3dea-757f-b84c-1ffd83d12b46-kyr-N3Uv4nDh-2026-01-15-21-48 directory

--- 019b9978-3dea-757f-b84c-1ffd83d12b46-kyr-N3Uv4nDh-2026-01-15-21-48/src/lib/services/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all services
-export * from './openFoodFacts';
-export * from './ingredientMatcher';
-export * from './ocr';
-export * from './aiExplanation';
--- 019b9978-3dea-757f-b84c-1ffd83d12b46-kyr-N3Uv4nDh-2026-01-15-21-48/src/lib/stores/index.ts
+++
@@ -1,5 +0,0 @@
-// Export all stores
-export { useUserStore } from './userStore';
-export { useHistoryStore } from './historyStore';
-export { useSubscriptionStore } from './subscriptionStore';
-export { useCompareStore } from './compareStore';
